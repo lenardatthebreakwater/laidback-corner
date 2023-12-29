8d40abc7234a1aba6872aed7cd34963d9f6fbbd1
Lenard <energeticbowlofrice@gmail.com>
2023-12-29 23:16:13 +0800

    Updated requirements.txt

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from .models import User, Post
