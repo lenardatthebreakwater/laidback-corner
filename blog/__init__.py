c58e1f8d65bad3e3929b418e4b69f1ffd59743ab
Lenard <energeticbowlofrice@gmail.com>
2023-12-29 23:05:01 +0800

    Added a databasetup.py file

--- blog/__init__.py
+++ blog/__init__.py
@@ -5,8 +5,8 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
+app.config['SECRET_KEY'] = 'samplesecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
